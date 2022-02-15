a7c58ef52ae1679d0400b0707559ec9d1aee7a1a
Alexander Slesarenko <avslesarenko@gmail.com>
2022-02-15 18:02:28 +0100

    update to Scala 3.1.1

--- src/main/scala/zeno/lang.scala
+++ src/main/scala/zeno/lang.scala
@@ -29,18 +29,21 @@ object lang {
   def let[S <: Type, T <: Type](t: Signal[S])(fn: Signal[S] => Signal[T]): Signal[T] =
     let("x", t)(fn)
 
-  def [S <: Type, T <: Type](t: Signal[S ~ T]) left: Signal[S] = Left(t)
+  extension [S <: Type, T <: Type](t: Signal[S ~ T])
+    def left: Signal[S] = Left(t)
+    def right: Signal[T] = Right(t)
 
-  def [S <: Type, T <: Type](t: Signal[S ~ T]) right: Signal[T] = Right(t)
+  extension [T <: Type](lhs: Signal[T])
+    def ~ [U <: Type](rhs: Signal[U]): Signal[T ~ U] = Pair(lhs, rhs)
 
-  def [T <: Type, U <: Type](lhs: Signal[T]) ~ (rhs: Signal[U]): Signal[T ~ U] = Pair(lhs, rhs)
+  extension (sig: Signal[_])
+    def as[T <: Type]: Signal[T] = sig.as[T]
 
-  def [T <: Type](sig: Signal[_]) as: Signal[T] = sig.as[T]
-
-  def [T <: Type](sig: Signal[T]) width: Int = sig.tpe match {
-    case VecT(width) => width
-    case _ => throw new Exception("access width on non-vector signal")
-  }
+  extension [T <: Type](sig: Signal[T])
+    def width: Int = sig.tpe match {
+      case VecT(width) => width
+      case _ => throw new Exception("access width on non-vector signal")
+    }
 
   def fsm[S <: Type, T <: Type](name: String, init: Value)(next: Signal[S] => Signal[S ~ T]): Signal[T] = {
     val tpInit = Values.typeOf(init)
@@ -60,21 +63,22 @@ object lang {
 
   def mux[T <: Type](cond: Signal[Bit], x: Signal[T], y: Signal[T]): Signal[T] = Mux(cond, x, y)
 
-  def [T <: Num](sig: Signal[Vec[T]]) unary_! : Signal[Vec[T]] = Not(sig)
+  extension [T <: Num](sig: Signal[Vec[T]])
+    def unary_! : Signal[Vec[T]] = Not(sig)
 
-  def [T <: Num](lhs: Signal[Vec[T]]) & (rhs: Signal[Vec[T]]): Signal[Vec[T]] = And(lhs, rhs)
+  extension [T <: Num](lhs: Signal[Vec[T]])
+    def & (rhs: Signal[Vec[T]]): Signal[Vec[T]] = And(lhs, rhs)
+    def | (rhs: Signal[Vec[T]]): Signal[Vec[T]] = Or(lhs, rhs)
+    def ^ (rhs: Signal[Vec[T]]): Signal[Vec[T]] = Or(And(lhs, !rhs), And(!lhs, rhs))
 
-  def [T <: Num](lhs: Signal[Vec[T]]) | (rhs: Signal[Vec[T]]): Signal[Vec[T]] = Or(lhs, rhs)
+  extension (value: Value)
+    def toSignal[T <: Type]: Signal[T] = value match {
+      case PairV(l, r) =>
+        (l.toSignal ~ r.toSignal).asInstanceOf
 
-  def [T <: Num](lhs: Signal[Vec[T]]) ^ (rhs: Signal[Vec[T]]): Signal[Vec[T]] = Or(And(lhs, !rhs), And(!lhs, rhs))
-
-  def [T <: Type](value: Value) toSignal: Signal[T] = value match {
-    case PairV(l, r) =>
-      (l.toSignal ~ r.toSignal).asInstanceOf
-
-    case VecV(bits)  =>
-      VecLit(bits).asInstanceOf
-  }
+      case VecV(bits)  =>
+        VecLit(bits).asInstanceOf
+    }
 
   inline def input[T <: Type](name: String): Var[T] =
     Var(Symbol(name), Types.typeOf[T])
@@ -85,36 +89,42 @@ object lang {
   implicit val lit0: Conversion[0, Signal[Bit]] = zero => Vec(0)
 
   /** Int -> Bits, take the least significant N bits */
-  def [N <: Num](n: Int) W(implicit M: ValueOf[N]): Signal[Vec[N]] = {
-    val N = M.value.asInstanceOf[Int]
-    val bits = (0 until N).foldLeft(Nil: List[0|1]) { (acc, i) =>
-      val bit: 0 | 1 = if (n & (1 << i)) == 0 then 0 else 1
-      bit :: acc
+  extension (n: Int)
+    def W[N <: Num](using M: ValueOf[N]): Signal[Vec[N]] = {
+      val N = M.value.asInstanceOf[Int]
+      val bits = (0 until N).foldLeft(Nil: List[0|1]) { (acc, i) =>
+        val bit: 0 | 1 = if (n & (1 << i)) == 0 then 0 else 1
+        bit :: acc
+      }
+      VecLit(bits)
     }
-    VecLit(bits)
-  }
 
   // ---------------- bit vector operations --------------------
 
-  def [M <: Num, N <: Num](vec: Signal[Vec[M]]) << (amount: Signal[Vec[N]]): Signal[Vec[M]] =
-    Shift(vec, amount, isLeft = true)
+  extension [M <: Num](vec: Signal[Vec[M]])
+    def <<[N <: Num](amount: Signal[Vec[N]]): Signal[Vec[M]] =
+      Shift(vec, amount, isLeft = true)
 
-  def [M <: Num, N <: Num](vec: Signal[Vec[M]]) >> (amount: Signal[Vec[N]]): Signal[Vec[M]] =
-    Shift(vec, amount, isLeft = false)
+    def >>[N <: Num](amount: Signal[Vec[N]]): Signal[Vec[M]] =
+      Shift(vec, amount, isLeft = false)
 
-  def [N <: Num](lhs: Signal[Vec[N]]) + (rhs: Signal[Vec[N]]): Signal[Vec[N]] =
-    Plus(lhs, rhs)
+    def apply(index: Int): Signal[Bit] = At(vec, index)
 
-  def [N <: Num](lhs: Signal[Vec[N]]) - (rhs: Signal[Vec[N]]): Signal[Vec[N]] =
-    Minus(lhs, rhs)
+  extension [N <: Num](lhs: Signal[Vec[N]])
+    def + (rhs: Signal[Vec[N]]): Signal[Vec[N]] =
+      Plus(lhs, rhs)
 
-  def [M <: Num, N <: Num, U <: Num](lhs: Signal[Vec[M]]) ++ (rhs: Signal[Vec[N]]): Signal[Vec[U]] = Concat(lhs, rhs)
+    def - (rhs: Signal[Vec[N]]): Signal[Vec[N]] =
+      Minus(lhs, rhs)
 
-  def [S <: Num](vec: Signal[Vec[S]]) apply(index: Int): Signal[Bit] = At(vec, index)
+  extension [M <: Num](lhs: Signal[Vec[M]])
+    def ++ [N <: Num, U <: Num](rhs: Signal[Vec[N]]): Signal[Vec[U]] = Concat(lhs, rhs)
 
-  def [S <: Num, U <: Num](vec: Signal[Vec[S]]) apply(to: Int, from: Int): Signal[Vec[U]] = Range[S, U](vec, to, from)
+  extension [S <: Num](vec: Signal[Vec[S]])
+    def apply[U <: Num](to: Int, from: Int): Signal[Vec[U]] = Range[S, U](vec, to, from)
 
-  def [T <: Num](x: Signal[Vec[T]]) === (y: Signal[Vec[T]]): Signal[Bit] = Equals(x, y)
+  extension [T <: Num](x: Signal[Vec[T]])
+    def === (y: Signal[Vec[T]]): Signal[Bit] = Equals(x, y)
 
   /** When syntax
    *
@@ -179,39 +189,44 @@ object lang {
     }
   }
 
-  def (lhs: Value) ~ (rhs: Value): Value =
-    new PairV(lhs, rhs)
+  extension (lhs: Value)
+    def ~ (rhs: Value): Value = new PairV(lhs, rhs)
 
   /** Int -> Bits */
-  def (n: Int) toValue(N: Int): Value = {
-    assert(N > 0 && N <= 32, "N = " + N + ", expect N > 0 && N <= 32")
-    assert(n >= 0, "n = " + n + ", expect n > 0") // TODO: no negative numbers for now
+  extension (n: Int)
+    def toValue(N: Int): Value = {
+      assert(N > 0 && N <= 32, "N = " + N + ", expect N > 0 && N <= 32")
+      assert(n >= 0, "n = " + n + ", expect n > 0") // TODO: no negative numbers for now
 
-    val bits = (0 until N).foldLeft(Nil: List[0|1]) { (acc, i) =>
-      val bit: 0 | 1 = if (n & (1 << i)) == 0 then 0 else 1
-      bit :: acc
+      val bits = (0 until N).foldLeft(Nil: List[0|1]) { (acc, i) =>
+        val bit: 0 | 1 = if (n & (1 << i)) == 0 then 0 else 1
+        bit :: acc
+      }
+
+      VecV(bits)
     }
 
-    VecV(bits)
-  }
+  extension (vec: Value)
+    def toInt: Int = vec match {
+      case vec: VecV =>
+        (0 until vec.width).foldLeft(0) { (acc, i) => acc | ((vec(i) & 1) << i) }
 
-  def (vec: Value) toInt: Int = vec match {
-    case vec: VecV =>
-      (0 until vec.width).foldLeft(0) { (acc, i) => acc | ((vec(i) & 1) << i) }
+      case _ =>
+        throw new Exception("Cannot call .toInt on pairs")
+    }
 
-    case _ =>
-      throw new Exception("Cannot call .toInt on pairs")
-  }
-
-  def [T <: Num](value: Value) toShort: Short = value.toInt.toShort
-
-  def [T <: Num](value: Value) toChar: Int = value.toInt.toChar
+  extension (value: Value)
+    def toShort: Short = value.toInt.toShort
+    def toChar: Int = value.toInt.toChar
 
   // ---------------- transform operations --------------------
 
-  def [T <: Type](sig: Signal[T]) show: String = Printing.show(sig)
+  extension [T <: Type](sig: Signal[T]) {
+    def show: String = Printing.show(sig)
 
-  def [T <: Type](sig: Signal[T]) eval(inputs: Var[_]*): List[Value] => Value = Interpreter.eval(inputs.toList, sig)
+    def eval(inputs: Var[_]*): List[Value] => Value = Interpreter.eval(inputs.toList, sig)
 
-  def [T <: Type](sig: Signal[T]) toVerilog(moduleName: String, inputs: Var[_]*): String = Verilog.emit(moduleName, inputs.toList, sig)
+    def toVerilog(moduleName: String,
+                  inputs: Var[_]*): String = Verilog.emit(moduleName, inputs.toList, sig)
+  }
 }
